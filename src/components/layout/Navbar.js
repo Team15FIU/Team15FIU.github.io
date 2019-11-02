b14792b28de3cce5424142c8cee67b91cd9d7d16
Elizabeth <elialume@gmail.com>
2019-11-02 01:44:52 -0400

    update

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom'
 import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
 import Search from './Search'
+import './../../../src/assets/img/'
 
 
 const Navbar = () => {
     return (
-        <nav className="nav-wrapper grey darken-3">
+        <nav className="nav-wrapper grey darken-3"
+       >
             <div className="container">
                 <Link to='/' className="brand-logo">BookBook NYC</Link>
                 <SignedInLinks />
