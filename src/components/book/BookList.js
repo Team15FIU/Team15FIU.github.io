65eed66f20e4272630d018aff874048d0542a7ef
Elizabeth <elialume@gmail.com>
2019-11-02 18:02:44 -0400

    Added sorting to book list

--- src/components/book/BookList.js
+++ src/components/book/BookList.js
@@ -17,6 +17,66 @@ const sortTypes = {
         if(book1.title > book2.title)
             return -1;
         return 0;
+    },
+    authorAsc: function(book1, book2){
+        if(book1.author < book2.author)
+            return -1;
+        if(book1.author > book2.author)
+            return 1;
+        return 0;
+    },
+    authorDesc: function(book1, book2){
+        if(book1.author < book2.author)
+            return 1;
+        if(book1.author > book2.author)
+            return -1;
+        return 0;
+    },
+    priceAsc: function(book1, book2){
+        if(book1.price < book2.price)
+            return -1;
+        if(book1.price > book2.price)
+            return 1;
+        return 0;
+    },
+    priceDesc: function(book1, book2){
+        if(book1.price < book2.price)
+            return 1;
+        if(book1.price > book2.price)
+            return -1;
+        return 0;
+    },
+    ratingAsc: function(book1, book2){
+        if(book1.rating < book2.rating)
+            return -1;
+        if(book1.rating > book2.rating)
+            return 1;
+        return 0;
+    },
+    ratingDesc: function(book1, book2){
+        if(book1.rating < book2.rating)
+            return 1;
+        if(book1.rating > book2.rating)
+            return -1;
+        return 0;
+    },
+    dateAsc: function(book1, book2){
+        let date1 = new Date(book1.releaseDate);
+        let date2 = new Date(book2.releaseDate);
+        if(date1 < date2)
+            return -1;
+        if(date1 > date2)
+            return 1;
+        return 0;
+    },
+    dateDesc: function(book1, book2){
+        let date1 = new Date(book1.releaseDate);
+        let date2 = new Date(book2.releaseDate);
+        if(date1 < date2)
+            return 1;
+        if(date1 > date2)
+            return -1;
+        return 0;
     }
 }
 
@@ -26,13 +86,29 @@ const getSorter = (sort) => {
             return sortTypes.titleAsc;
         case 'titleDesc':
             return sortTypes.titleDesc;
+        case 'authorAsc':
+            return sortTypes.authorAsc; 
+        case 'authorDesc':
+            return sortTypes.authorDesc;  
+        case 'priceAsc':
+            return sortTypes.priceAsc; 
+        case 'priceDesc':
+            return sortTypes.priceDesc; 
+        case 'ratingAsc':
+            return sortTypes.ratingAsc; 
+        case 'ratingDesc':
+            return sortTypes.ratingDesc; 
+        case 'dateAsc':
+            return sortTypes.dateAsc; 
+        case 'dateDesc':
+            return sortTypes.dateDesc; 
         default:
             return (() => 0)
     }
 }
 
-const BookList = ({books, sort}) => {
-    if(books && sort != 'none'){
+const BookList = ({books, sort, filter}) => {
+    if(books){
         books.sort(getSorter(sort))
     }
 
@@ -40,7 +116,8 @@ const BookList = ({books, sort}) => {
         <div className="project-list section">
             { books && books.map(book => {
                 return(
-                    <Link to={'/book/' + book.id}>
+                    <Link style={{display: "inline-block", width: "100%", maxWidth: "250px"}} 
+                        to={'/book/' + book.id}>
                      <BookSummary book={book} key={book.id}/>
                     </Link>
                 )
